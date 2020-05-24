b376156cdf8e01b27cd7008bb377b26c4220a3b7
yeahlowflicker <yeahlowflicker@gmail.com>
2020-05-24 18:13:21 +0800

    v2020.e.18 | Made Elements Reusable
    ----
    
    Version: 2020.e.18
    Date: May 24, 2020   18:14   GMT+8
    
    ----
    
    Elements such as navbar and footer are now included in pages via JavaScript
    code in order to achieve reusability.
    
    ----
    
    Meta tag of all pages are added with content attributes to disable browser
    caches and force reload of content when the server content changes.

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+var html = '\
+          <link rel="stylesheet" href="/css/footer.css">\
+          <footer>\
+               <section class="footer-links">\
+                    <a href="https://www.instagram.com/yeahlowflickerproduction" target="_blank">Instagram</a>\
+                    <a href="https://www.youtube.com/channel/UCyOZNX6yoHb-vBhthVBKzQg" target="_blank">YouTube</a>\
+                    <a href="https://www.github.com/YeahlowflickerProduction" target="_blank">GitHub</a>\
+               </section>\
+               <label>Copyright © 2020 Yeahlowflicker Production</label>\
+          </footer>';
+
+
+document.getElementById("footer").innerHTML = html;
---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+var html = '\
+          <link rel="stylesheet" href="/css/navbar.css">\
+          <section class="logo">\
+               <img src="/res/YFV4_transparent.png">\
+               <h1>Yeahlowflicker Production</h1>\
+          </section>\
+          \
+          <section class="navbar-links">\
+               <a href="/about.html">About Us</a>\
+               <a href="/scripts.html">Scripts</a>\
+               <a href="/projects.html">Projects</a>\
+               <a href="/index.html">Home</a>\
+          </section>';
+
+
+document.getElementById("navbar").innerHTML = html;
