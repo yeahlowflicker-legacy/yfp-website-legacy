25b1af2c7eaad24b539157c12c38eaff67cfe76c
yeahlowflicker <yeahlowflicker@gmail.com>
2020-06-23 21:11:22 +0800

    v2020.f.6 | Updated Homepage Images & Layouts

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,6 @@
 var html = '\
           <link rel="stylesheet" href="/css/navbar.css">\
+          \
           <section class="logo">\
                <a href="/index.html"><img src="/res/YFV4_transparent.png"></a>\
                <a href="/index.html"><h1>Yeahlowflicker Production</h1></a>\
@@ -11,7 +12,30 @@ var html = '\
                <a href="/projects.html">Projects</a>\
                <a href="/products.html">Products</a>\
                <a href="/index.html">Home</a>\
-          </section>';
+          </section>\
+          \
+          <div id="sideNav">\
+               <a href="/index.html">Home</a>\
+               <a href="/products.html">Products</a>\
+               <a href="/projects.html">Projects</a>\
+               <a href="/scripts.html">Scripts</a>\
+               <a href="/about.html">About Us</a>\
+               <br><br><br><br><br><a onclick="openNav()">X</a>\
+          </div>';
 
 
 document.getElementById("navbar").innerHTML = html;
+
+
+var sideNavOpened = false;
+
+
+function openNav() {
+     if (sideNavOpened)
+          document.getElementById("sideNav").style.width = "0";
+     else 
+          document.getElementById("sideNav").style.width = "25%";
+
+     sideNavOpened = !sideNavOpened;
+}
+
